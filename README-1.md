b2abdfe5ac4b92aee1c4963da330f5b222ec4949
pramasamy65 <prakashkec@gmail.com>
2019-05-12 09:15:50 +0530

    Adding Spring Cloud Config

---
+++ README-1.md
@@ -0,0 +1,9 @@
+## MicroServices
+
+
+
+## Spring Cloud Config Server and Config Client Example
+
+### Refer spring-cloud-config-server project
+	* https://springbootdev.com/2018/07/14/microservices-introduction-to-spring-cloud-config-server-with-client-examples/
+	* 
--- spring-cloud-config-server/src/test/java/com/example/handson/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
+++ spring-cloud-config-server/src/test/java/com/example/handson/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.handson.movieratingservice;
+package com.example.handson.springcloudconfigserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MovieRatingServiceApplicationTests {
+public class SpringCloudConfigServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
