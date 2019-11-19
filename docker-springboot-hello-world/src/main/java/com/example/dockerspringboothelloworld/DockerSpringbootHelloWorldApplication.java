df744f5cb2f060527761d5979d52635089d61b9f
pramasamy65 <prakashkec@gmail.com>
2019-11-19 07:30:26 +0530

    Helloworld Docker Program

---
+++ docker-springboot-hello-world/src/main/java/com/example/dockerspringboothelloworld/DockerSpringbootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerspringboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringbootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringbootHelloWorldApplication.class, args);
+	}
+
+}
---
+++ docker-springboot-hello-world/src/main/java/com/example/dockerspringboothelloworld/HelloDockerRestController.java
@@ -0,0 +1,17 @@
+package com.example.dockerspringboothelloworld;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloDockerRestController {
+	@RequestMapping("/hello/{name}")
+	public String helloDocker(@PathVariable(value = "name") String name) {
+		String response = "Hello " + name + " Response received on : " + new Date();
+		System.out.println(response);
+		return response;
+	}
+}
